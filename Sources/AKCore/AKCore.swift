661938b3b2ff6a6b6d3de685a9ccdd63b9d3ef90
Aubree Kendall <ajkendall@mac.com>
2019-12-14 18:34:12 -0500

    Testing public some more

--- Sources/AKCore/AKCore.swift
+++ Sources/AKCore/AKCore.swift
@@ -1,8 +1,19 @@
 public struct AKCore {
-    var text = "Hello, World!"
     public var public_var = "public var"
-    static let static_let = "static let"
     public static let public_static_let = "public static let"
+
+    public init() {
+
+    }
+
+    public func akcoreShow(message: String) {
+        print(message)
+    }
+
+}
+
+public func show(message: String) {
+    print(message)
 }
 
 public let akcoretest = "public let"
